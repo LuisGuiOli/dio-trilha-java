12cd33d5172f2fc7cf52e6bf2b2ebe6f1a7512cc
Luis Guilherme Nunes Lima de Oliveira <guigonl16@gmail.com>
2025-08-01 02:44:05 -0300

    Código básico de interção com usuário

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
+    
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor,  digite o seu nome: "); 
+        String nameClient = scanner.nextLine();
+        System.out.println("Por favor,  digite o número da sua Agência: "); 
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor,  digite o número da sua conta: "); 
+        int numConta = scanner.nextInt();
+        System.out.println("Por favor,  digite o seu saldo: "); 
+        float saldo = scanner.nextFloat();
+        
+        
+        
+
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nameClient, agencia, numConta, saldo);
+        scanner.close();
     }
+
+    
 }
